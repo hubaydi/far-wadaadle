39915b30ee2c04b95eb781378ae685a2524e068c
hobaydi <cajaaib252@gmail.com>
2026-04-07 16:11:23 +0300

    feat: initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,52 @@
+export const LATIN_TO_FAR_WADAAD = new Map<string, string>([
+  ["b", "ب"],
+  ["t", "ت"],
+  ["j", "ج"],
+  ["x", "ح"],
+  ["kh", "خ"],
+  ["d", "د"],
+  ["r", "ر"],
+  ["s", "س"],
+  ["sh", "ش"],
+  ["dh", "ڎ"],
+  ["c", "ع"],
+  ["g", "گ"],
+  ["f", "ف"],
+  ["q", "ق"],
+  ["k", "ك"],
+  ["l", "ل"],
+  ["m", "م"],
+  ["n", "ن"],
+  ["h", "ه"],
+  ["w", "و"],
+  ["y", "ي"],
+  ["a", "\u064E"], // Fatha َ
+  ["e", "\u065A"], // Small V Below ٚ
+  ["i", "\u0650"], // Kasra ِ
+  ["o", "\u0654"], // Inverted Damma ٗ
+  ["u", "\u064F"], // Damma ُ
+  ["aa", "ا"],
+  ["ee", "ێ"],
+  ["ii", "ي"],
+  ["oo", "وٗ"],
+  ["uu", "ۇ"],
+]);
+
+export const punctuationMap = new Map<string, string>([
+  ["?", "؟"], // Arabic question mark
+  [",", "،"], // Arabic comma
+  [";", "؛"], // Arabic semicolon
+]);
+
+export const numberMap = new Map<string, string>([
+  ["0", "٠"],
+  ["1", "١"],
+  ["2", "٢"],
+  ["3", "٣"],
+  ["4", "٤"],
+  ["5", "٥"],
+  ["6", "٦"],
+  ["7", "٧"],
+  ["8", "٨"],
+  ["9", "٩"],
+]);
